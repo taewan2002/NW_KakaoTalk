0a0f7dbb512f62e7b5065d1f760ce410dd4f52b7
kimuijin <whitekuj02@naver.com>
2022-12-12 14:38:24 +0900

    코인데이터 화면 form수정

--- src/GUI/chattingRoom.java
+++ src/GUI/chattingRoom.java
@@ -11,6 +11,10 @@ public class chattingRoom extends JFrame {
     private String user_id;
     private chatting_client client;
     private ListeningThread t1;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton fileOption;
+
     public chattingRoom(String user_id, chatting_client client, ListeningThread t1){
         this.user_id = user_id;
         this.client = client;
